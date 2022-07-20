1baefa387430c2834bdd3d8a8dda231ed48988a8
Justin Petrovich <24559515+justin-petrovich@users.noreply.github.com>
2022-07-20 00:14:37 -0400

    Delete Simulations directory

--- Simulations/mse.R
+++
@@ -1,9 +0,0 @@
-mse <- function(grid,grid.obs,beta,betahat){
-  if(length(beta)!=length(betahat)){
-    notobs <- setdiff(grid,grid.obs)
-    id <- match(notobs,grid)
-    beta <- beta[-id]
-  }
-  K <- length(beta)
-  sum((beta - betahat)^2)/K
-}
